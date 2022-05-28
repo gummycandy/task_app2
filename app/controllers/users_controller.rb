9d2a6da37a9779d589fa6a0a7c93dd73fd2a1c53
EC2 Default User <ec2-user@ip-172-31-9-55.ap-northeast-1.compute.internal>
2022-05-28 14:11:09 +0000

    ログインページ作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def login
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to("/users/show")
+    else
+      render("users/new")
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :mail_address, :password_digest)
+  end
+
+end
